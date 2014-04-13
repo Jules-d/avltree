b4c9e4e535806600ee2788611bbaf219ec67682e
Jules <julian.debhal@gmail.com>
2014-04-13 14:53:55 +1000

    initial commit

---
+++ spec/avltree/core_spec.clj
@@ -0,0 +1,9 @@
+(ns avltree.core-spec
+    (:use
+        [speclj.core]
+        [avltree.core]))
+
+(describe "AVL Tree"
+
+    (it ""
+        (should true)))
---
+++ src/avltree/core.clj
@@ -0,0 +1,10 @@
+(ns avltree.core)
+
+(defn -main
+    "I don't do a whole lot."
+    [& args]
+    println ("Hello, World!"))
+
+(defn testSpec
+    []
+    true)
