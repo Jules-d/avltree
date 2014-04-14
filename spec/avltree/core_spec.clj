075ac6afce0f97d38b2163e43975ff94f4f6bfad
Jules <julian.debhal@gmail.com>
2014-04-14 21:51:06 +1000

    AVL tree 1.0

--- spec/avltree/core_spec.clj
+++ spec/avltree/core_spec.clj
@@ -41,4 +41,86 @@
           (it "right-right"
               (should= 6 (get-in @left-leaning-5-tree [:right :right :value])))
           (it "should handle multiple values"
-              (should= @left-leaning-5-tree (insert 3 2 5 1 4 6))))
+              (should= @left-leaning-5-tree (insert 3 2 5 1 4 6)))
+;          (it "should always be a binary tree"              (should= :true (validate-bst (insert (insert 3 1 4 2 6 5)))))
+          )
+
+(describe "search"
+          (with balanced-odd-numbers-tree (insert 7
+                                                  11 3
+                                                  9 13
+                                                  5 1))
+          (it "returns nil for empty trees"
+              (should= nil (search nil 7) ))
+          (it "returns nil for values not in the tree"
+              (should= nil (search @balanced-odd-numbers-tree 12)))
+          (it "returns values in the tree"
+              (should= 7 (search @balanced-odd-numbers-tree 7)))
+          (it "returns a value on the left"
+              (should= 1 (search @balanced-odd-numbers-tree 1)))
+          (it "returns values anywhere in the tree"
+              (should= 3 (search @balanced-odd-numbers-tree 3))))
+
+(describe "rotate left"
+          (with unrotated-left-1-2-3 {:value 1,
+                                      :right {:value 2,
+                                              :right {:value 3, :height 0}}})
+          (with rotated-left-1-2-3 (rotate-left @unrotated-left-1-2-3))
+          (it "new root node" (should= 2 (:value @rotated-left-1-2-3)))
+          (it "new left node" (should= 1 (:value (:left @rotated-left-1-2-3))))
+          (it "new right node" (should= 3 (:value (:right @rotated-left-1-2-3)))))
+
+(describe "rotate right"
+          (with unrotated-3-2-1 {:value 3 :left {:value 2 :left {:value 1}}})
+          (with rotated-right-3-2-1 (rotate-right @unrotated-3-2-1))
+          (it "new root node"
+              (should= 2 (:value @rotated-right-3-2-1)))
+          (it "new left node"
+              (should= 1 (:value (:left @rotated-right-3-2-1))))
+          (it "new right node"
+              (should= 3 (:value (:right @rotated-right-3-2-1)))))
+
+(describe "recalculate-height"
+          (it "assocs 1 with no children"
+              (should= 1 (:height (recalculate-height (insert 1)))))
+          (it "assocs 2 with a child"
+              (should= 2 (:height (recalculate-height (insert 1 2))))))
+
+(describe "height"
+          (it "height of nil is 0"
+              (should= 0 (height nil)))
+          (it "a new node has height 1"
+              (should= 1 (height (insert 1))))
+          (it "a node with a child has height 2"
+              (should= 2 (height (insert 1 2)))))
+
+(describe "balance-factor"
+          (it "has a balance-factor of 1 after a left-insert"
+              (should= 1 (balance-factor (insert 3 2))))
+          (it "has a balance-factor of -1 after a right-insert"
+              (should= -1 (balance-factor (insert 2 3))))
+          (it "has a balance-factor of 0 after a left and a right insert"
+              (should= 0 (balance-factor (insert 2 3 1))))
+          (it "still has a balance-factor of zero after multiple balance-factord inserts"
+              (should= 0 (balance-factor (insert 7 11 3 9 5 13 1))))
+          (it "will rebalance-factor at 2 (left left)"
+              (should= 2 (:value (insert 3 2 1)))))
+
+
+(describe "balance"
+          (it "handles the left-left case"
+              (should= 2 (:value (insert 3 2 1)))
+              (should= 1 (:value (:left (insert 3 2 1))))
+              (should= 3 (:value (:right (insert 3 2 1)))))
+          (it "should handle the left-right case"
+              (should= 2 (:value (insert 3 1 2)))
+              (should= 1 (:value (:left (insert 3 2 1))))
+              (should= 3 (:value (:right (insert 3 2 1)))))
+          (it "should handle the right-right case"
+              (should= 2 (:value (insert 1 2 3)))
+              (should= 1 (:value (:left (insert 3 2 1))))
+              (should= 3 (:value (:right (insert 3 2 1)))))
+          (it "should handle the right-left case"
+              (should= 2 (:value (insert 1 3 2)))
+              (should= 1 (:value (:left (insert 3 2 1))))
+              (should= 3 (:value (:right (insert 3 2 1))))))
--- src/avltree/core.clj
+++ src/avltree/core.clj
@@ -1,22 +1,103 @@
 (ns avltree.core)
 
 (defn create-tree [value]
-  {:value value})
-
-(defn insert
-  ([tree value]
-      (cond (nil? tree) (create-tree value)
-            (number? tree) (insert (create-tree tree) value)
-            (= value (:value tree)) tree
-            (< value (:value tree)) (assoc tree :left (insert (:left tree) value))
-            (< (:value tree) value) (assoc tree :right (insert (:right tree) value))
-            :true :error))
-  ([tree value & values]
-     (apply insert (cons (insert tree value) values))))
+  {:value value :height 1})
 
 (defn search
   [tree value]
-  (cond (nil? tree) nil))
+  (cond (nil? tree) nil
+        (= value (:value tree)) value
+        (< value (:value tree)) (search (:left tree) value)
+        (< (:value tree) value) (search (:right tree) value)
+        :true :error))
+
+(defn height [tree]
+  (cond (nil? tree) 0
+        (contains?  tree :height) (:height tree)
+        :true (inc (max (height (:left tree))
+                        (height (:right tree))))))
+
+(defn recalculate-height [tree]
+  (assoc tree :height (inc (max (height (:left tree))
+                                (height (:right tree))))))
+
+(defn rotate-left [tree]
+  (let [A (:value tree)
+        B (:value (:right tree))
+        X (:left tree)
+        Y (get-in tree [:right :left])
+        Z (get-in tree [:right :right])]
+    ;(println "rotating left")
+    {:value B
+     :left {:value A :left X :right Y}
+     :right Z}))
+
+(defn rotate-right [tree]
+  (let [B (:value tree)
+        A (:value (:left tree))
+        Z (:right tree)
+        X (:left (:left tree))
+        Y (:right (:left tree))]
+    ;(println "rotating right")
+    (recalculate-height
+     {:value A
+      :left X
+      :right (recalculate-height
+              {:value B
+               :left Y
+               :right Z})})))
+
+
+(defn balance-factor [tree]
+  (if (nil? tree) 0 (- (height (:left tree)) (height (:right tree)))))
+
+(defn balance [tree]
+  (let [local-balance-factor (balance-factor tree)]
+         ;(println (:value potentially-unbalanced-result) " balance: " balance)
+    (cond (= 2 local-balance-factor) (cond
+                                (= 1 (balance-factor (:left tree))) (rotate-right tree) ;; left left case
+                                (= -1 (balance-factor (:left tree)))
+                                (rotate-right
+                                 (assoc tree :left (rotate-left (:left tree)) ))
+                                :true [:rotate-right-error (balance-factor (:left tree))])
+          (= -2 local-balance-factor) (if (= -1 (balance-factor (:right tree)))
+                                        (rotate-left tree) ; right right case
+                                        (rotate-left
+                                         (assoc tree :right (rotate-right (:right tree)))))
+          :true tree))
+
+  )
+
+(defn insert*
+  "The outer insert checks that the value is not already in the tree so we know we're definitely inserting something here."
+  ([tree value]
+     (let [potentially-unbalanced-result
+           (cond (nil? tree) (create-tree value)
+                 (< value (:value tree)) (recalculate-height
+                                          (assoc tree
+                                            :left (insert* (:left tree) value)))
+                 (< (:value tree) value) (recalculate-height
+                                          (assoc tree
+                                            :right (insert* (:right tree) value)))
+                 :true :error)]
+       (balance potentially-unbalanced-result))))
+
+(defn insert
+  ([tree-or-value]
+     (if (number? tree-or-value)
+       (create-tree tree-or-value)
+       tree-or-value))
+  ([tree value]
+      (cond
+       (number? tree) (insert (create-tree tree) value)
+       (search tree value) tree
+       :true (insert* tree value)))
+  ([tree value & values]
+     (apply insert (cons (insert tree value) values))))
+
+(defn validate-bst [tree]
+  nil)
+
 
 (defn -main
     "I don't do a whole lot."
